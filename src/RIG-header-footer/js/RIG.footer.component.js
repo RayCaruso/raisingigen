b56e656430ce0f70ba056f6870505ab11f71fd3b
Ray Caruso <caruso.ray@gmail.com>
2017-11-23 14:40:59 -0700

    non sticky navbar. Angular based components for header and footer across whole site.

---
+++ src/RIG-header-footer/js/RIG.footer.component.js
@@ -0,0 +1,10 @@
+(function () {
+'use strict';
+
+  angular.module('RaisingIGenApp')
+  .component('rigFooter', {
+    templateUrl: "src/RIG-header-footer/templates/footer-template.html"
+    }
+  );
+
+})();
---
+++ src/RIG-header-footer/js/RIG.header.component.js
@@ -0,0 +1,10 @@
+(function () {
+'use strict';
+
+  angular.module('RaisingIGenApp')
+  .component('rigMenuHeader', {
+    templateUrl: "src/RIG-header-footer/templates/head-template.html"
+    }
+  );
+
+})();
